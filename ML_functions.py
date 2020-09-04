6b076e7d94de3761154336f93e474ca6b8c9b87e
salomaestro <chris10an.salomonsen@gmail.com>
2020-09-04 12:54:27 +0200

    Added a new file which contains the unneccesary functions

--- ML_functions.py
+++ ML_functions.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-
-def power_iteration_v3(matrix, N_sim):
+def power_iteration(matrix, i):
 	"""
 	Iterates like the power iteration method to converge on the stationary probabilities of the transitionmatrix
 
@@ -11,56 +10,6 @@ def power_iteration_v3(matrix, N_sim):
 	Returns:
 		(ndarray) - Stationary probability matrix
 	"""
-	la = np.random.rand(matrix.shape[1])
-
-	for _ in range(N_sim):
-	
-		la1 = np.dot(matrix, la)
-		
-		la1_norm = np.linalg.norm(la1, 1)
-		
-		la = la1 / la1_norm
-
-	return la
-
-def power_iteration_v2(matrix):
-	"""
-	Iterates like the power iteration method to converge on the stationary probabilities of the transitionmatrix,
-	but when the difference is small enough 
-
-	Args:
-		param1: (ndarray) - Transition matrix
-	Returns:
-		(ndarray) - Stationary probability matrix
-		(float) - eigenvalue
-		(int) - number of iteratios
-	"""
-	# creates a vector of random float values as a row vector with the same size of rows as given matrix
-	v = np.random.rand(matrix.shape[1])
-
-	# finds eigenvalues
-	ev = matrix.dot(v)
-	ev = v.dot(ev)
-
-	iterations = 0
-
-	while True:
-		Av = matrix.dot(v)
-		v_new = Av / np.linalg.norm(Av, 1)
-
-		ev_new = matrix.dot(v_new)
-		ev_new = v_new.dot(ev_new)
-
-		if np.abs(ev - ev_new) < 1E-15:
-			break
-
-		v = v_new
-		ev = ev_new
-		iterations += 1
-
-	return v_new, ev_new, iterations
-
-def power_iteration(matrix, i):
 	pi = np.random.rand(matrix.shape[1])
 	pi_norm = pi / np.linalg.norm(pi, 1)
 
@@ -76,7 +25,7 @@ def PageRank(matrix):
 	Args:
 		(numpy.matrix) - Transitionmatrix
 	Returns:
-		(ndarray) - ranked 
+		(ndarray) - ranked
 	"""
 	ev = power_iteration(matrix, 100)
 	ev = np.array(ev)[0]
@@ -89,7 +38,7 @@ def ModifiedPageRank(matrix, alpha=0.85):
 	"""
 	Googles implementation of the PageRank method, Uses the principle G = alpha * S + (1 - alpha) * E
 	The ordering goes from 0 which is the best result, to n which is the worst result
-	
+
 	Args:
 		param1: (ndarray) - matrix, should be of size n x n
 		param2: (float) - optional, 0 < alpha < 1, determines how zeros and ones should be converted to floats, follows links, else, URL.
--- ukesset2.py
+++ ukesset2.py
@@ -2,17 +2,17 @@ import numpy as np
 from ML_functions import power_iteration, power_iteration_v2, power_iteration_v3, PageRank, ModifiedPageRank
 
 Pt = np.transpose(np.array(
-	[[.03, .19, .27, .51], 
-	[.24, .23, .33, .20], 
-	[.21, .42, .19, .18], 
+	[[.03, .19, .27, .51],
+	[.24, .23, .33, .20],
+	[.21, .42, .19, .18],
 	[.22, .34, .08, .36]]
 	))
 
 # (a)
 Amatrix = np.array(
-	[[0, 0, 1, .5], 
-	[1/3, 0, 0, 0], 
-	[1/3, .5, 0, .5], 
+	[[0, 0, 1, .5],
+	[1/3, 0, 0, 0],
+	[1/3, .5, 0, .5],
 	[1/3, .5, 0, 0]])
 #(b)
 Bmatrix = np.array(
@@ -23,15 +23,13 @@ Bmatrix = np.array(
 	[0, 0, 0, 0, 0]])
 # 2b
 Amatrix_modified = np.array(
-	[[0, 0, .5, .5, 0], 
-	[1/3, 0, 0, 0, 0], 
-	[1/3, .5, 0, .5, 1], 
+	[[0, 0, .5, .5, 0],
+	[1/3, 0, 0, 0, 0],
+	[1/3, .5, 0, .5, 1],
 	[1/3, .5, 0, 0, 0],
 	[0, 0, 0, .5, 0]])
 
 print(power_iteration(Pt, 20))
-print(power_iteration_v2(Pt)[0])
-print(power_iteration_v3(Pt, 20))
 aNaive = PageRank(Amatrix)
 aModified = ModifiedPageRank(Amatrix)
 bNaive = PageRank(Bmatrix)
