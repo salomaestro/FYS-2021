be689ec8edda2037de15c2228c890c7979528386
salomaestro <chris10an.salomonsen@gmail.com>
2020-09-10 00:00:35 +0200

    ..

--- ukesset3.py
+++ ukesset3.py
@@ -38,12 +38,8 @@ reg = LinearRegression(data)
 result = reg.least_squares()
 
 #### scipy comparison
-<<<<<<< HEAD
 slope, intercept, r_value, p_value, std_err = stats.linregress(data)
 y = slope * data[1] + intercept
-=======
-# slope, intercept, r_value, p_value, std_err = stats.linregress(data.T[0, :], data.T[1, :])
->>>>>>> 2e1c32c18423e5948d4a01a966ea47be9d3794c1
 ####
 
 plt.plot(reg.x, reg.y, ".")
