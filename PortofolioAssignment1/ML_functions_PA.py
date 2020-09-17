50b08b9fc0203559a0a3737b31a5868df28564b3
salomaestro <chris10an.salomonsen@gmail.com>
2020-09-17 11:44:09 +0200

    added mlfunctions

---
+++ PortofolioAssignment1/ML_functions_PA.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+def power_iteration(matrix, i):
+	"""
+	Iterates like the power iteration method to converge on the stationary probabilities of the transitionmatrix
+
+	Args:
+		param1: (ndarray) - Transition matrix
+		parma2: (int) - Number of iterations
+	Returns:
+		(ndarray) - Stationary probability matrix
+	"""
+	pi = np.random.rand(matrix.shape[1])
+	pi_norm = pi / np.linalg.norm(pi, 1)
+
+	pow_matrix = np.matrix(matrix) ** i
+
+	return pow_matrix.dot(pi_norm)
+
+def PageRank(matrix):
+	"""
+	Uses the PageRank method to list most relevant suggestions ordered
+	The ordering goes from 0 which is the best result, to n which is the worst result.
+
+	Args:
+		(numpy.matrix) - Transitionmatrix
+	Returns:
+		(ndarray) - ranked
+	"""
+	ev = power_iteration(matrix, 100)
+	ev = np.array(ev)[0]
+	temp = ev.argsort()
+	ranks = np.empty_like(temp)
+	ranks[temp] = np.arange(len(ev))[::-1]
+	return ranks
+
+def ModifiedPageRank(matrix, alpha=0.85):
+	"""
+	Googles implementation of the PageRank method, Uses the principle G = alpha * S + (1 - alpha) * E
+	The ordering goes from 0 which is the best result, to n which is the worst result
+
+	Args:
+		param1: (ndarray) - matrix, should be of size n x n
+		param2: (float) - optional, 0 < alpha < 1, determines how zeros and ones should be converted to floats, follows links, else, URL.
+
+	Returns:
+		(ndarray) - list of ranks
+	"""
+	# Should implement a system for not using the first row if all elements are zero.
+	# if H[0,:].all():
+	# 	pass
+
+	a = np.zeros(np.shape(matrix)[0])
+	a[0] = 1 / (np.count_nonzero(matrix.T[0,:]) + 1)
+
+
+	# Concatenates a new row vector to the bottom of the original matrix
+	H = np.concatenate((matrix, [a]))
+	firstRow = H.T[0,:]
+	firstRow[firstRow > 0] = a[0]
+
+	# Concatenates a new row with 1/N to the bottom of the transposed matrix H
+	b = np.ones(np.shape(H)[0]) / (np.shape(H)[0])
+	S = np.concatenate((H.T, [b]))
+
+	E = np.ones(np.shape(S)) / np.shape(S)[0]
+
+	# G is the Google matrix
+	G = alpha * S + (1 - alpha) * E
+
+	# Now the ranking begins
+	return PageRank(G.T)
+
+def main():
+	return None
+
+if __name__ == "__name__":
+	main()
--- PortofolioAssignment1/coderepo/problem1.py
+++ PortofolioAssignment1/coderepo/problem1.py
@@ -1,69 +1,71 @@
 import numpy as np
+import os
+import ML_functions
 
-games = np.genfromtxt("FYS-2021\PortofolioAssignment1\coderepo\chess-games.csv", delimiter=" ")
-names = np.genfromtxt("FYS-2021\PortofolioAssignment1\coderepo\chess-games-names.csv", delimiter=",", dtype=str)
+#creating arrays of the data we are to process
+games = np.genfromtxt("coderepo/chess-games.csv", delimiter=" ")
+names = np.genfromtxt("coderepo/chess-games-names.csv", delimiter=",", dtype=str)
 
+# extracting the id, and singular names out of the names array.
 id, name = names.T
 id = id.astype("int32")
-# rows are i, columns are j. aij
 
-
+#constructing an matrix A of correct size and shape
 A = np.zeros((np.shape(id)[0], np.shape(id)[0]))
-index = np.arange(0, np.shape(games)[0])
+
+#looping throught each game such that we see them as i.e: [0, 1, 0.0] as the first game,
+# with index: ind.
+for ind, _ in enumerate(games):
+    #assigning
+     white = games[ind][0].astype("int32")
+     black = games[ind][1].astype("int32")
+     score = games[ind][2]
+     #if white loses assign one loss to whites row, index black
+     if score == 0:
+         A[white][black] += 1
+    #do the opposite for blacks row, index white
+     elif score == 1:
+         A[black][white] += 1
+
+#dividing the elements of each row by the sum of the row
+A = A / np.sum(A, axis=1).reshape((-1, 1))
+print(A)
 
 
-white_won = np.where(games.T[2] == 1) # index til runden
-black_won = np.where(games.T[2] == 0) # index til runden
-tie = np.where(games.T[2] == 0.5)
 
 
-i, j, _ = games[white_won].T.astype("int32")
-k, l, _ = games[black_won].T.astype("int32")
 
 
-print(i)
 
 
-A[i, j] += 1
-A[l, k] += 1
 
+
+#A = np.zeros((np.shape(id)[0], np.shape(id)[0]))
+#index = np.arange(0, np.shape(games)[0])
+#white_won = np.where(games.T[2] == 1) # index of the current game
+#black_won = np.where(games.T[2] == 0) # index of the current game
+#tie = np.where(games.T[2] == 0.5)
+#i, j, _ = games[white_won].T.astype("int32")
+#k, l, _ = games[black_won].T.astype("int32")
+#print(i, k)
+#print(np.size(i), "size", np.size(k))
+#A[j][i] += 1
+#A[k][l] += 1
+#print(np.sum(A))
+#print(A[171, 115])
+#print(A)
+#print(A[115, 171])
+#print(A[171, 115])
+#A / np.sum(A, axis=1)
+#print(np.sum(A, axis=1))
 ######################
 #Still needs a way to divide each row on the total losses of the row.
 ######################
-
-
 # print(A[1] / np.sum(A[1]))
-mat_ind = np.arange(0, np.shape(A)[0])
-print(mat_ind.shape)
-
-A[mat_ind] = A[mat_ind] / np.sum(A[mat_ind])
+#mat_ind = np.arange(0, np.shape(A)[0])
+#print(mat_ind.shape)
+#A[mat_ind] = A[mat_ind] / np.sum(A[mat_ind])
 # print(A)
-
-
-
 # print(np.shape(mat_ind))
 # A[games[white_won]] += 1
-print(np.count_nonzero(A))
-
-
-
-
-
-
-
-
-
-#
-# A = np.zeros((np.shape(id)[0], np.shape(id)[0]))
-#
-# for ind, _ in enumerate(games):
-#     white = games[ind][0].astype("int32")
-#     black = games[ind][1].astype("int32")
-#     score = games[ind][2]
-#     if score == 1:
-#         A[white][black] += 1
-#     elif score == 0:
-#         A[black][white] += 1
-# print(A)
-#
-# print(np.count_nonzero(A))
+#print(np.count_nonzero(A))
