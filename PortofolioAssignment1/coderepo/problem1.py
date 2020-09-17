5eedc1e0feffe21dccf00d4f9a104449e92e9aad
salomaestro <chris10an.salomonsen@gmail.com>
2020-09-17 16:23:07 +0200

    cleanup

--- PortofolioAssignment1/coderepo/problem1.py
+++ PortofolioAssignment1/coderepo/problem1.py
@@ -2,50 +2,64 @@ import numpy as np
 import os
 from ML_functions_PA import power_iteration, PageRank
 
-#creating arrays of the data we are to process
-games = np.genfromtxt("coderepo/chess-games.csv", delimiter=" ")
-names = np.genfromtxt("coderepo/chess-games-names.csv", delimiter=",", dtype=str)
+# fixing the file path
+dirname = os.path.dirname(__file__)
+filename_game = os.path.join(dirname, "chess-games.csv")
+filename_names = os.path.join(dirname, "chess-games-names.csv")
 
+# creating arrays of the data we are to process
+games = np.genfromtxt(filename_game, delimiter=" ")
+names = np.genfromtxt(filename_names, delimiter=",", dtype=str)
+
+#################### Problem (1f) #################################
 # extracting the id, and singular names out of the names array.
 id, name = names.T
 id = id.astype("int32")
 
-#constructing an matrix A of correct size and shape
-A = np.zeros((np.shape(id)[0], np.shape(id)[0]))
+def RankChessGames(playedGames, usedNames):
+    # constructing an matrix A of correct size and shape
+    A = np.zeros((np.shape(id)[0], np.shape(id)[0]))
 
-#looping throught each game such that we see them as i.e: [0, 1, 0.0] as the first game,
-# with index: ind.
-for ind, _ in enumerate(games):
-    #assigning
-     white = games[ind][0].astype("int32")
-     black = games[ind][1].astype("int32")
-     score = games[ind][2]
-     #if white loses assign one loss to whites row, index black
-     if score == 0:
-         A[white][black] += 1
-    #do the opposite for blacks row, index white
-     elif score == 1:
-         A[black][white] += 1
+    # looping throught each game such that we see them as i.e: [0, 1, 0.0] as the first game,
+    # with index: ind.
+    for ind, _ in enumerate(playedGames):
+        # assigning rows of the playedGames to white, black and the score
+        white = playedGames[ind][0].astype("int32")
+        black = playedGames[ind][1].astype("int32")
+        score = playedGames[ind][2]
+        # if white loses assign one loss to whites row, index black
+        if score == 0:
+            A[white][black] += 1
+        # do the opposite for blacks row, index white
+        elif score == 1:
+            A[black][white] += 1
 
-#dividing the elements of each row by the sum of the row
-A = A / np.sum(A, axis=1).reshape((-1, 1))
+    # dividing the elements of each row by the sum of the row
+    A = A / np.sum(A, axis=1).reshape((-1, 1))
 
-ranks = PageRank(A.T)
-ranks = np.flip(ranks)
-rankid = ranks[0:10]
+    # using the page rank method of the imported python-file: ML_functions_PA.py
+    ranks = PageRank(A.T)
 
-idname = names[rankid]
+    # flip to get the desired output at the top of the array
+    ranks = np.flip(ranks)
 
-crr_shape = np.shape(idname[1])[0]
-list_of = np.arange(1, crr_shape + 1).astype("str")
+    # pick out only the top ten id's
+    rankid = ranks[0:10]
 
-def prettyprint():
+    # find the top ten id's in usedNames
+    idname = usedNames[rankid]
+
+    # Cute print ;)
     string = ""
     string += "rank, id, name"
     for i in range(len(idname)):
         string += "\n" + str(i + 1) + ", " + str(idname[i][0]) + ", " + str(idname[i][1])
-    print(string)
-prettyprint()
+    return string
+
+result = RankChessGames(games, names)
+print(result)
+
+################################# Problem (1g) ###############################################
 
 
 
