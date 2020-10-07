e687c2ff78380709a28caf2629d749cb50472615
salomaestro <chris10an.salomonsen@gmail.com>
2020-10-08 01:37:04 +0200

    k

--- ukesset5/ukessett5_oppg1.py
+++ ukesset5/ukessett5_oppg1.py
@@ -21,28 +21,38 @@ repeat
         w_j = w_j + nu * delta w_j
 until convergence
 """
+# Import and initialize the data
 data = np.genfromtxt(filename_tictac, delimiter=" ", dtype="int64")
 groundTruth = data[:, 0]
 tactoes = np.delete(data, 0, axis=1)
 
 def gradDescent(X, r):
+    # Define random vector of weights with the same dimension as our data, 9 in this case
     w = np.random.uniform(-0.01, 0.01, size=(X.shape[1]))
-    w0 = np.random.uniform(0, 10)
-    s = 0.5
 
+    # Define random bias
+    w0 = 100
+
+    # Step size, may change later.
+    s = 5
+
+    i = np.zeros(np.shape(X)[0])
+    # Iterate throught each result (row of data)
     for t in range(0, np.shape(X)[0]):
-        dj = np.zeros_like(w)
+        # Create array of ones with same shape as the weights this is the derivatives
+        Dj = np.ones_like(w) * 10
         grddesc = True
         while grddesc:
-            test = np.where(dj < 0.5, 1, 0)
+            # Sigmoid
+            y = 1 / (1 + np.exp(-(np.dot(w.T, X[t]) + w0)))
+            Dj = (r[t] - y) * X[t]
+            w = w + s * Dj
+            w0 = w0 + s * r[t] - y
+            test = np.where(Dj < 0.05, 1, 0)
             if np.all((test == 1)):
                 grddesc = False
-            y = 1 / (1 + np.exp(-(np.dot(w, X[t]) + w0)))
-            dj = np.sum(r[t] - y) * X[t]
-            print(abs(dj))
-            w = w + s * dj
-            w0 = w0 + s * np.sum(r[t] - y)
+            i[t] += 1
 
-
-    c = np.dot(w.T, X) + w0
+    c = np.dot(w, X.T) + w0
+    print(c[c < 0].shape)
 gradDescent(tactoes, groundTruth)
