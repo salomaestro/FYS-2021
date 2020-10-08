41fa7f9645d9c493ba87e30ac7b41ed324d16b0b
salomaestro <chris10an.salomonsen@gmail.com>
2020-10-08 13:28:05 +0200

    finished the trainingalgorithm, now i should use it on test data, althoug not important.

--- ukesset5/ukessett5_oppg1.py
+++ ukesset5/ukessett5_oppg1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+
 dirname = os.path.dirname(__file__)
 filename_tictac = os.path.join(dirname, "tictac-end.csv")
 
@@ -7,10 +8,57 @@ filename_tictac = os.path.join(dirname, "tictac-end.csv")
 data = np.genfromtxt(filename_tictac, delimiter=" ", dtype="int64")
 np.random.shuffle(data)
 
-groundTruth = data[:, 0]
-tactoes = np.delete(data, 0, axis=1)
+# How many percent of our data we would like to use for training the algorithm
+trainingpercent = 0.7
+trainingpercentdata = np.floor(data.shape[0] * trainingpercent).astype("int64")
 
-sigmoid = lambda x, weights, bias: 1 / (1 + np.exp(-(np.matmul(x, weights) + bias)))
+# Training data is the first 70% of the whole dataset, then test is the remainder
+trainingData = data[0 : trainingpercentdata]
+testdata = data[trainingpercentdata - 1 : -1]
+
+# Storing the ground truth in a separate array
+groundTruth = trainingData[:, 0]
+
+# Choosing the relevant data by adding a column of ones where we remove the ground truth column.
+onearr = np.ones_like(groundTruth)
+tactoes = np.delete(trainingData, 0, axis=1)
+tactoes = np.insert(tactoes, 0, onearr, axis=1)
+
+# Define a sigmoid function.
+sigmoid = lambda x, weights: 1 / (1 + np.exp(-(np.matmul(x, weights))))
+
+def gradDescent(X, r):
+    # Define random vector of weights with the same dimension as our data, 9 in this case
+    w = np.random.uniform(-0.01, 0.01, size=(X.shape[1])).astype("float128")
+
+    # Step size, may change later.
+    s = 0.05
+
+    # Create array of ones with same shape as the weights this is the derivatives
+    Dj = np.ones_like(w) * 10
+    grddesc = True
+
+    while grddesc:
+        y = sigmoid(X, w)
+        Dj = np.matmul((r - y), X)
+        w = w + s * Dj
+
+        test = np.where(Dj < 0.01, 1, 0)
+
+        # Test when convergence
+        if np.all((test == 1)):
+            grddesc = False
+
+    c = np.round(sigmoid(X, w))
+    return c
+
+classification = gradDescent(tactoes, groundTruth)
+
+classifiedZero = np.where(classification < 0.5)
+classifiedOne = np.where(classification > 0.5)
+
+actualZero = np.where(groundTruth < 0.5)
+actualOne = np.where(groundTruth > 0.5)
 
 def confusionMatrix(classified0, classified1, actual0, actual1):
     """
@@ -30,49 +78,8 @@ def confusionMatrix(classified0, classified1, actual0, actual1):
     tn = np.intersect1d(classified1, actual1).shape[0]
     return np.array([np.array([tp, fn]), np.array([fp, tn])])
 
-def gradDescent(X, r):
-    # Define random vector of weights with the same dimension as our data, 9 in this case
-    # w = np.ones(1)
-    # w = np.concatenate((w, np.random.uniform(-0.01, 0.01, size=(X.shape[1]))))
+confusionmatrix = confusionMatrix(classifiedZero, classifiedOne, actualZero, actualOne)
 
-    w = np.random.uniform(-0.01, 0.01, size=(X.shape[1]))
-    # Define random bias
-    w0 = np.random.uniform(-0.01, 0.01)
+accuracy = (confusionmatrix[0][0] + confusionmatrix[1][1]) / np.sum(confusionmatrix).astype("float32")
 
-    # Step size, may change later.
-    s = 5
-
-    # Iterate throught each result (row of data)
-    for t in range(0, np.shape(X)[0]):
-        # Create array of ones with same shape as the weights this is the derivatives
-        Dj = np.ones_like(w) * 10
-        grddesc = True
-
-        while grddesc:
-
-            y = sigmoid(X, w, w0)
-            Dj = (r[t] - y[t]) * X[t]
-            w = w + s * Dj
-            w0 = w0 + s * r[t] - y[t]
-            test = np.where(Dj < 0.01, 1, 0)
-
-            # Test when convergence
-            if np.all((test == 1)):
-                grddesc = False
-
-    c = np.round(sigmoid(X, w, w0))
-
-    classifiedZero = np.where(c < 0.5)
-    classifiedOne = np.where(c > 0.5)
-
-    actualzero = np.where(r < 0.5)
-    actualone = np.where(r > 0.5)
-
-    confusionmatrix = confusionMatrix(classifiedZero, classifiedOne, actualzero, actualone)
-
-    print(np.sum(confusionmatrix), confusionmatrix[0][0], confusionmatrix[1][1])
-
-    accuracy = (confusionmatrix[0][0] + confusionmatrix[1][1]) / np.sum(confusionmatrix)
-
-    print(accuracy)
-gradDescent(tactoes, groundTruth)
+print("This algorithm gives the confusion matrix: \n{:},\n and has an accuracy of {:}.".format(confusionmatrix, accuracy))
