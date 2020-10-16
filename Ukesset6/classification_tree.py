34d4f18c75fa9296f44d39b442a97e3f39f15a6f
salomaestro <chris10an.salomonsen@gmail.com>
2020-10-16 18:27:54 +0200

    made a bit of insights but no real progress.

--- Ukesset6/classification_tree.py
+++ Ukesset6/classification_tree.py
@@ -1,34 +1,72 @@
 import numpy as np
+import os
+
+dirname = os.path.dirname(__file__)
+filename_blobs = os.path.join(dirname, "blobs.csv")
+
+# Import and initialize the data
+data = np.genfromtxt(filename_blobs, delimiter=" ", dtype="float32")
+
+def exctractFromData(dataset, trainpercent=0.7):
+    # How many percent of our data we would like to use for training the algorithm
+    trainpercentdata = np.floor(dataset.shape[0] * trainpercent).astype("int64")
+
+    # Training data is the first x % of the whole dataset, then test is the remainder
+    trainData = dataset[0 : trainpercentdata]
+    testData = dataset[trainpercentdata - 1 : -1]
+
+    groundTruth = trainData[:, 0].astype("int64")
+    # Remove ground truth from trainingdata
+    trainData = np.delete(trainData, 0, axis=1)
+    testData = np.delete(testData, 0, axis=1)
+
+    return trainData, testData, groundTruth
+
+trainingData, testingData, groundTruth = exctractFromData(data)
+
+
+
+c0 = data[np.where(data[:, 0] == 0)]
+c1 = data[np.where(data[:, 0] == 1)]
+
+splits_attr1 = np.linspace(np.min(data[:, 1]), np.max(data[:, 1]), num=100)
+splits_attr2 = np.linspace(np.min(data[:, 2]), np.max(data[:, 2]), num=100)
+
+
 
 class Tree:
     """
     Method for classifying with a decision tree approach
     """
-    def __init__(self, dataset):
-        # Data structured as ndarray([groundtruth, feature1, feature2, ... featureN])
-        self.data = dataset
-        np.random.shuffle(self.data)
-        trainingpercent = 0.7
-        trainingpercentdata = np.floor(self.data.shape[0] * trainingpercent).astype("int64")
-        self.trainingData = self.data[0 : trainingpercentdata]
-        self.tesData = self.data[trainingpercentdata - 1 : -1]
-        self.ground_truth = self.trainingData[:, 0]
+    def __init__(self, train, test, ground_truth, data):
+        # init data, training, test and ground truth.
+        self.train = train
+        self.test = test
+        self.gt = ground_truth
+        self.data = data
 
         # Settings
         self.minimum_impurity = 1
 
-        # Init node
-        self.firstNode = np.random.uniform()
+        # # Init node
+        # self.firstNode = np.random.uniform()
+
+    def splitAttribute(self):
+        c0 = data[np.where(data[:, 0] == 0)]
+        c1 = data[np.where(data[:, 0] == 1)]
+
+        splits_attr1 = np.linspace(np.min(data[:, 1]), np.max(data[:, 1]), num=100)
+        splits_attr2 = np.linspace(np.min(data[:, 2]), np.max(data[:, 2]), num=100)
+
+        entropy = split_entropy(splits_attr1, splits_attr2)
 
 
-    def impurity(self, probMat):
+
+    def split_entropy(self, probMat):
+        # Total entropy/impurity after the split
+        impurity = np.sum(self.data)
+
+
+    def nodeEntropy(self, probMat):
         I = - np.sum(probMat * np.log2(probMat))
         return I
-
-    def total_entropy(self, probMat):
-        # Total entropy/impurity after the split
-        pass
-
-    def fit(self):
-        if impurity(self.data) < minimum_impurity
-            # declare leaf node
