126afe9d4c3ce3149a8d484610e1a941ee012525
salomaestro <chris10an.salomonsen@gmail.com>
2020-11-12 11:31:42 +0100

    Finished code. General logic, plotting and labels are correct now.

--- Ukesset7/oppgave2.py
+++ Ukesset7/oppgave2.py
@@ -1,8 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os
-from dataloader import load_data
+from dataloader import load_data, load_text
 
 data = load_data("city-distances.csv")
+names = load_text("city-names.csv")
 
 class FeatureReduction:
     def __init__(self, data):
@@ -17,18 +19,48 @@ class FeatureReduction:
         Returns:
             (ndarray) - result
         """
-        X = self.data
-        eigenvals_n_vecs = np.linalg.eig(np.matmul(X.T, X))
-        D = eigenvals_n_vecs[0]
-        D = np.identity(len(D)) * D
-        
+        # square proximity matrix
+        SQPROXMAT = self.data ** 2
 
+        onemat = np.ones_like(SQPROXMAT)
 
+        # Applying double centering since data is a proximity matrix
+        J = np.identity(len(SQPROXMAT)) - 1 / len(SQPROXMAT) * onemat
+        B = - 0.5 * np.matmul(np.matmul(J, SQPROXMAT), J)
 
+        # find eigenvalues and vectors
+        eigenvals_n_vecs = np.linalg.eig(B)
+
+        # sort then give indice of largest
+        largest_eigenvals_indices = np.flip(np.argsort(eigenvals_n_vecs[0]))
+
+        # find the d largest eigen indices
+        wanted_eigenvals = largest_eigenvals_indices[0 : dimensions]
+
+        # get the largest eigenvalues and vectors
+        ev = eigenvals_n_vecs[0][wanted_eigenvals]
+        evec = eigenvals_n_vecs[1][:, wanted_eigenvals]
+
+        Lambda = np.identity(len(ev)) * ev
+        Z = np.matmul(evec, Lambda ** 0.5)
+        return Z
+
+def plotcity(data, names):
+    fig, axs = plt.subplots()
+    axs.scatter(data[:, 0], data[:, 1])
+
+    # add names
+    for i, name in enumerate(names):
+        axs.annotate(name, data[i])
+
+    plt.ylim([-750, 1000])
+    plt.title("Norway")
+    plt.show()
 
 def main():
     mds = FeatureReduction(data)
     res = mds.MDS(2)
+    plotcity(res, names)
 
 if __name__ == "__main__":
     main()
