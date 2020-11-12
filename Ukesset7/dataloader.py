5e7f90b992519ed82e42d1002aa797bb90c2489e
salomaestro <chris10an.salomonsen@gmail.com>
2020-11-12 11:30:25 +0100

    Added a function for importing text-files.

--- Ukesset7/dataloader.py
+++ Ukesset7/dataloader.py
@@ -11,3 +11,14 @@ def load_data(*filenames, delimiter=" "):
         return np.asarray(data[0])
     else:
         return np.asarray(data)
+
+def load_text(*filenames):
+    dirname = os.path.dirname(__file__)
+    data = []
+    for filename in filenames:
+        filename_joined = os.path.join(dirname, str(filename))
+        data.append(np.genfromtxt(filename_joined, delimiter="\n", dtype=str))
+    if len(data) == 1:
+        return np.asarray(data[0])
+    else:
+        return np.asarray(data)
